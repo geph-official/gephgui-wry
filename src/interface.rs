69c47a8e662537e8070014479abe43f8ed1c031c
nullchinchilla <nullchinchilla@pm.me>
2021-10-10 12:49:32 -0500

    lala

--- src/interface.rs
+++ src/interface.rs
@@ -28,6 +28,7 @@ pub fn global_rpc_handler(window: &Window, req: RpcRequest) -> Option<RpcRespons
         "set_conversion_factor" => {
             handle_rpc(req, |params| handle_set_conversion_factor(window, params))
         }
+        "get_url" => handle_rpc(req, handle_get_url),
         other => {
             tracing::error!("unrecognized RPC verb {}", other);
             None
@@ -152,6 +153,15 @@ fn handle_set_conversion_factor(window: &Window, params: (f64,)) -> anyhow::Resu
     });
     window.set_resizable(false);
     Ok("".into())
+}
+
+/// Handles a request to poll a particular URL
+#[tracing::instrument]
+fn handle_get_url(params: (String,)) -> anyhow::Result<String> {
+    smol::future::block_on(async move {
+        let mut resp = surf::get(params.0).await.map_err(|e| e.into_inner())?;
+        resp.body_string().await.map_err(|e| e.into_inner())
+    })
 }
 
 fn handle_rpc<I: DeserializeOwned, O: Serialize, F: FnOnce(I) -> anyhow::Result<O>>(
