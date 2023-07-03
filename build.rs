2cd28ace0d4ebbb3fae1b7230557b56aaf2a815d
antidmg <antidmg@proton.me>
2023-07-03 13:41:37 -0500

    add manifest for window admin prompting

--- build.rs
+++ build.rs
@@ -5,6 +5,9 @@ use winres::WindowsResource;
 fn main() -> io::Result<()> {
     #[cfg(windows)]
     {
+        // This embeds a Windows manifest into the Rust executable to prompt the user for administrator privileges.
+        embed_resource::compile("gephgui-wry-manifest.rc", embed_resource::NONE);
+
         WindowsResource::new()
             // This path can be absolute, or relative to your crate root.
             .set_icon("src/logo-naked.ico")
